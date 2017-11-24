97c8352c43d8fb16d9b11d605fecc49fb594d81a
Serhii Rubets <serhii_rubets@epam.com>
2017-11-24 18:46:28 +0200

    first

---
+++ models/Store.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const slug = require('slugs');
+
+mongoose.Promise = global.Promise;
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
+});
+
+storeSchema.pre('save', (next) => {
+  if (!this.isModified('name')) {
+    next();
+    return;
+  }
+
+  this.slug = slug(this.name);
+  next();
+});
+
+module.exports = mongoose.model('Store', storeSchema);
