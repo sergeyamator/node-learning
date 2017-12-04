f0cc46c46d265998a30ec7d654c7fa22808b8d2f
Serhii Rubets <serhii_rubets@epam.com>
2017-12-04 19:06:45 +0200

    Edit store ability was added

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
 
 exports.addStore = (req, res) => {
   res.render('editStore', {title: 'Add store'});
+};
+
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  req.flash('success', `Successfully Created ${store.name}`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', {title: 'stores', stores});
 };
